965f92ced8640df2092c75a599596ec42e8a9027
Damientl <damien.tll@gmail.com>
2025-10-06 16:19:01 +0200

    fix player

--- piano_lib/audioPlayer.py
+++ piano_lib/audioPlayer.py
@@ -1,8 +1,20 @@
-from playsound import playsound
+import pygame
+import time
 
 class AudioPlayer:
     def __init__(self, audios):
+        pygame.mixer.init()
         self.audios = audios
+        self.loaded_sounds = []
+        for audio_file in self.audios:
+            print("Loading audio: " + "assets/" + audio_file)
+            sound = pygame.mixer.Sound("assets/" + audio_file)
+            self.loaded_sounds.append(sound)
     
     def play_audio(self, index):
-        playsound("assets/" + self.audios[index])
+        print("Playing audio: " + "assets/" + self.audios[index])
+        sound = self.loaded_sounds[index]
+        sound.play()
+        # Wait for the sound to finish playing
+        while pygame.mixer.get_busy():
+            time.sleep(0.1)
