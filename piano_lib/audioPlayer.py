b2c033cad91b86f7351f686bce920a0ab128c21f
Damientl <damien.tll@gmail.com>
2025-10-06 16:24:00 +0200

    Revert "fix player"
    This reverts commit 965f92ced8640df2092c75a599596ec42e8a9027.

--- piano_lib/audioPlayer.py
+++ piano_lib/audioPlayer.py
@@ -1,20 +1,8 @@
-import pygame
-import time
+from playsound import playsound
 
 class AudioPlayer:
     def __init__(self, audios):
-        pygame.mixer.init()
         self.audios = audios
-        self.loaded_sounds = []
-        for audio_file in self.audios:
-            print("Loading audio: " + "assets/" + audio_file)
-            sound = pygame.mixer.Sound("assets/" + audio_file)
-            self.loaded_sounds.append(sound)
     
     def play_audio(self, index):
-        print("Playing audio: " + "assets/" + self.audios[index])
-        sound = self.loaded_sounds[index]
-        sound.play()
-        # Wait for the sound to finish playing
-        while pygame.mixer.get_busy():
-            time.sleep(0.1)
+        playsound("assets/" + self.audios[index])
